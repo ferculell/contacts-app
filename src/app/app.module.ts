4e43d86b46748909f4750c53affe28d8b9f6a789
Fernando Culell <ferculell@gmail.com>
2021-03-26 20:24:55 -0300

    menu para botones de filas agregado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
 
 import { ConexionService } from './services/conexion.service';
 import { ListaComponent } from './components/lista/lista.component'
@@ -34,7 +35,8 @@ import { ListaComponent } from './components/lista/lista.component'
     MatFormFieldModule,
     MatInputModule,
     MatButtonModule,
-    MatIconModule
+    MatIconModule,
+    MatMenuModule
   ],
   providers: [ConexionService],
   bootstrap: [AppComponent]
