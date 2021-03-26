dad5936d105f840c94375c3511afb3bf5e10f9bc
Fernando Culell <ferculell@gmail.com>
2021-03-26 14:29:51 -0300

    se eliminaron algunas lineas comentadas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,8 @@ import { environment } from '../environments/environment';
 import { MatTableModule } from '@angular/material/table';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 import { ConexionService } from './services/conexion.service';
 import { ListaComponent } from './components/lista/lista.component'
@@ -30,7 +32,9 @@ import { ListaComponent } from './components/lista/lista.component'
     AngularFirestoreModule,
     MatTableModule,
     MatFormFieldModule,
-    MatInputModule
+    MatInputModule,
+    MatButtonModule,
+    MatIconModule
   ],
   providers: [ConexionService],
   bootstrap: [AppComponent]
--- src/app/components/lista/lista.component.ts
+++ src/app/components/lista/lista.component.ts
@@ -1,27 +1,3 @@
-/* 
-import { Component, OnInit } from '@angular/core';
-import { ConexionService } from 'src/app/services/conexion.service';
-
-@Component({
-  selector: 'app-lista',
-  templateUrl: './lista.component.html',
-  styleUrls: ['./lista.component.css']
-})
-export class ListaComponent implements OnInit {
-
-  items:any;
-
-  constructor(private conexion:ConexionService) { 
-    this.conexion.listarItems().subscribe(item => {
-      this.items = item;
-    })
-  }
-
-  ngOnInit(): void {
-  }
-
-} */
-
 import { Component, OnInit } from '@angular/core';
 import { ConexionService } from 'src/app/services/conexion.service';
 import { MatTableDataSource } from '@angular/material/table';
